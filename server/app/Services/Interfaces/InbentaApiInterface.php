bf5899e371faea765d8c961f8b9d43a128cbced6
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2020-04-20 16:48:09 +0200

    Uses deleted

--- server/app/Services/Interfaces/InbentaApiInterface.php
+++ server/app/Services/Interfaces/InbentaApiInterface.php
@@ -2,12 +2,6 @@
 
 namespace App\Services\Interfaces;
 
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-use App\Http\Requests\SendMessageRequest;
-
-use Carbon\Carbon;
-
 interface InbentaApiInterface 
 {
     
